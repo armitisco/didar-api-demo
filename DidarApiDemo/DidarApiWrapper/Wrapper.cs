7271952ea10afa4c73d2442e1a1dd2d2b6dcd901
sinasava <sina.savaedi@gmail.com>
2015-04-11 18:02:54 +0430

    Very Basic API call

---
+++ DidarApiDemo/DidarApiWrapper/Wrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DidarApiWrapper.me.didar;
+
+namespace DidarApi
+{
+	public static class Wrapper
+	{
+		public static async Task<BizDomainData> GetBizDomainData(string apiKey)
+		{
+			using (CollaborativeSoapClient client = new CollaborativeSoapClient())
+			{
+				GetBizDomainResponse response = await client.GetBizDomainAsync(apiKey);
+				return response.Body.GetBizDomainResult.Data;
+			}
+		}
+	}
+}
