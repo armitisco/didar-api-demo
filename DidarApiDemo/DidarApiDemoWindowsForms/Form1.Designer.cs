c8f148e4e8a7f34c9c556e74afa19aa962f53088
sinasava <sina.savaedi@gmail.com>
2015-04-27 11:48:26 +0430

    Initial requests added

--- DidarApiDemo/DidarApiDemoWindowsForms/Form1.Designer.cs
+++ DidarApiDemo/DidarApiDemoWindowsForms/Form1.Designer.cs
@@ -28,34 +28,124 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
 			this.apiKeyTextBox = new System.Windows.Forms.TextBox();
+			this.getBizDomainDataButton = new System.Windows.Forms.Button();
+			this.getUsersButton = new System.Windows.Forms.Button();
+			this.getContactsButton = new System.Windows.Forms.Button();
+			this.titleTextBox = new System.Windows.Forms.TextBox();
+			this.firstNameTextBox = new System.Windows.Forms.TextBox();
+			this.lastNameTextBox = new System.Windows.Forms.TextBox();
+			this.mobilePhoneTextBox = new System.Windows.Forms.TextBox();
+			this.addContactButton = new System.Windows.Forms.Button();
+			this.workPhoneTextBox = new System.Windows.Forms.TextBox();
+			this.workPhoneExtensionTextBox = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(70, 77);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// apiKeyTextBox
 			// 
-			this.apiKeyTextBox.Location = new System.Drawing.Point(70, 12);
+			this.apiKeyTextBox.Location = new System.Drawing.Point(12, 12);
 			this.apiKeyTextBox.Name = "apiKeyTextBox";
-			this.apiKeyTextBox.Size = new System.Drawing.Size(202, 20);
-			this.apiKeyTextBox.TabIndex = 1;
+			this.apiKeyTextBox.Size = new System.Drawing.Size(280, 20);
+			this.apiKeyTextBox.TabIndex = 0;
+			// 
+			// getBizDomainDataButton
+			// 
+			this.getBizDomainDataButton.Location = new System.Drawing.Point(12, 38);
+			this.getBizDomainDataButton.Name = "getBizDomainDataButton";
+			this.getBizDomainDataButton.Size = new System.Drawing.Size(280, 23);
+			this.getBizDomainDataButton.TabIndex = 1;
+			this.getBizDomainDataButton.Text = "Get BizDomain Data";
+			this.getBizDomainDataButton.UseVisualStyleBackColor = true;
+			this.getBizDomainDataButton.Click += new System.EventHandler(this.getBizDomainDataButton_Click);
+			// 
+			// getUsersButton
+			// 
+			this.getUsersButton.Location = new System.Drawing.Point(12, 67);
+			this.getUsersButton.Name = "getUsersButton";
+			this.getUsersButton.Size = new System.Drawing.Size(280, 23);
+			this.getUsersButton.TabIndex = 2;
+			this.getUsersButton.Text = "Get Users";
+			this.getUsersButton.UseVisualStyleBackColor = true;
+			this.getUsersButton.Click += new System.EventHandler(this.getUsersButton_Click);
+			// 
+			// getContactsButton
+			// 
+			this.getContactsButton.Location = new System.Drawing.Point(12, 96);
+			this.getContactsButton.Name = "getContactsButton";
+			this.getContactsButton.Size = new System.Drawing.Size(280, 23);
+			this.getContactsButton.TabIndex = 3;
+			this.getContactsButton.Text = "Get Contacts";
+			this.getContactsButton.UseVisualStyleBackColor = true;
+			this.getContactsButton.Click += new System.EventHandler(this.getContactsButton_Click);
+			// 
+			// titleTextBox
+			// 
+			this.titleTextBox.Location = new System.Drawing.Point(12, 125);
+			this.titleTextBox.Name = "titleTextBox";
+			this.titleTextBox.Size = new System.Drawing.Size(60, 20);
+			this.titleTextBox.TabIndex = 4;
+			// 
+			// firstNameTextBox
+			// 
+			this.firstNameTextBox.Location = new System.Drawing.Point(78, 125);
+			this.firstNameTextBox.Name = "firstNameTextBox";
+			this.firstNameTextBox.Size = new System.Drawing.Size(100, 20);
+			this.firstNameTextBox.TabIndex = 5;
+			// 
+			// lastNameTextBox
+			// 
+			this.lastNameTextBox.Location = new System.Drawing.Point(184, 125);
+			this.lastNameTextBox.Name = "lastNameTextBox";
+			this.lastNameTextBox.Size = new System.Drawing.Size(100, 20);
+			this.lastNameTextBox.TabIndex = 6;
+			// 
+			// mobilePhoneTextBox
+			// 
+			this.mobilePhoneTextBox.Location = new System.Drawing.Point(12, 151);
+			this.mobilePhoneTextBox.Name = "mobilePhoneTextBox";
+			this.mobilePhoneTextBox.Size = new System.Drawing.Size(109, 20);
+			this.mobilePhoneTextBox.TabIndex = 7;
+			// 
+			// addContactButton
+			// 
+			this.addContactButton.Location = new System.Drawing.Point(12, 176);
+			this.addContactButton.Name = "addContactButton";
+			this.addContactButton.Size = new System.Drawing.Size(280, 23);
+			this.addContactButton.TabIndex = 8;
+			this.addContactButton.Text = "Add Contact";
+			this.addContactButton.UseVisualStyleBackColor = true;
+			this.addContactButton.Click += new System.EventHandler(this.addContactButton_Click);
+			// 
+			// workPhoneTextBox
+			// 
+			this.workPhoneTextBox.Location = new System.Drawing.Point(127, 151);
+			this.workPhoneTextBox.Name = "workPhoneTextBox";
+			this.workPhoneTextBox.Size = new System.Drawing.Size(104, 20);
+			this.workPhoneTextBox.TabIndex = 9;
+			// 
+			// workPhoneExtensionTextBox
+			// 
+			this.workPhoneExtensionTextBox.Location = new System.Drawing.Point(237, 151);
+			this.workPhoneExtensionTextBox.Name = "workPhoneExtensionTextBox";
+			this.workPhoneExtensionTextBox.Size = new System.Drawing.Size(47, 20);
+			this.workPhoneExtensionTextBox.TabIndex = 10;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.ClientSize = new System.Drawing.Size(304, 266);
+			this.Controls.Add(this.workPhoneExtensionTextBox);
+			this.Controls.Add(this.workPhoneTextBox);
+			this.Controls.Add(this.addContactButton);
+			this.Controls.Add(this.mobilePhoneTextBox);
+			this.Controls.Add(this.lastNameTextBox);
+			this.Controls.Add(this.firstNameTextBox);
+			this.Controls.Add(this.titleTextBox);
+			this.Controls.Add(this.getContactsButton);
+			this.Controls.Add(this.getUsersButton);
+			this.Controls.Add(this.getBizDomainDataButton);
 			this.Controls.Add(this.apiKeyTextBox);
-			this.Controls.Add(this.button1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
@@ -65,8 +155,17 @@
 
 		#endregion
 
-		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.TextBox apiKeyTextBox;
+		private System.Windows.Forms.Button getBizDomainDataButton;
+		private System.Windows.Forms.Button getUsersButton;
+		private System.Windows.Forms.Button getContactsButton;
+		private System.Windows.Forms.TextBox titleTextBox;
+		private System.Windows.Forms.TextBox firstNameTextBox;
+		private System.Windows.Forms.TextBox lastNameTextBox;
+		private System.Windows.Forms.TextBox mobilePhoneTextBox;
+		private System.Windows.Forms.Button addContactButton;
+		private System.Windows.Forms.TextBox workPhoneTextBox;
+		private System.Windows.Forms.TextBox workPhoneExtensionTextBox;
 	}
 }
 
--- DidarApiDemo/DidarApiDemoWindowsForms/Form1.cs
+++ DidarApiDemo/DidarApiDemoWindowsForms/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DidarApiWrapper.me.didar;
+using System.Web.Script.Serialization;
 
 namespace DidarApiDemoWindowsForms
 {
@@ -18,15 +19,104 @@ namespace DidarApiDemoWindowsForms
 			InitializeComponent();
 		}
 
-		private void button1_Click(object sender, EventArgs e)
+		private static string ToJson(object obj)
 		{
-			GetBizDomainData();
+			return new JavaScriptSerializer().Serialize(obj);
+		}
+
+		private void getBizDomainDataButton_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				GetBizDomainData();
+			}
+			catch (Exception ex)
+			{
+				HandleError(ex);
+			}
+		}
+
+		private void HandleError(Exception ex)
+		{
+			MessageBox.Show(ex.Message);
+		}
+
+		private void getUsersButton_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				GetUsers();
+			}
+			catch (Exception ex)
+			{
+				HandleError(ex);
+			}
+		}
+
+		private void getContactsButton_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				GetContacts();
+			}
+			catch (Exception ex)
+			{
+				HandleError(ex);
+			}
 		}
 
 		private async void GetBizDomainData()
 		{
 			BizDomainData data = await DidarApi.Wrapper.GetBizDomainData(apiKeyTextBox.Text);
+			MessageBox.Show(ToJson(data));
+		}
+
+		private async void GetUsers()
+		{
+			BizDomainUser[] data = await DidarApi.Wrapper.GetUsersData(apiKeyTextBox.Text);
+			MessageBox.Show(ToJson(data));
+		}
+
+		private async void GetContacts()
+		{
+			Contact[] data = await DidarApi.Wrapper.GetContactsData(apiKeyTextBox.Text, DateTime.Now.Date.AddDays(-10));
+			MessageBox.Show(ToJson(data));
+		}
+
+		private void addContactButton_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				AddContact();
+			}
+			catch (Exception ex)
+			{
+				HandleError(ex);
+			}
+		}
+
+		private async void AddContact()
+		{
+			BizDomainUser[] users = await DidarApi.Wrapper.GetUsersData(apiKeyTextBox.Text);
+			var rand = new Random();
+			var user = users[rand.Next(users.Length)];
+
+			Contact contact = new Contact()
+			{ 
+				OwnerId = user.Id,
+				Title = titleTextBox.Text,
+				FirstName = firstNameTextBox.Text,
+				LastName = lastNameTextBox.Text,
+				MobilePhone = mobilePhoneTextBox.Text,
+				WorkPhone = workPhoneTextBox.Text,
+				WorkPhoneExtension = workPhoneExtensionTextBox.Text
+			};
+
+			contact = await DidarApi.Wrapper.AddContact(apiKeyTextBox.Text, contact);
+
+			MessageBox.Show(ToJson(contact));
 			
 		}
+
 	}
 }
